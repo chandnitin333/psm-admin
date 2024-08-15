7c0b6e32fdc2e4517ed437557127aa0dbb9e9078
Kundan Kotangale <kundanmkotangale@gmail.com>
2024-08-15 09:21:12 +0530

    update menus and dashboard icon

--- src/app/modules/admin/components/home/home.component.ts
+++ src/app/modules/admin/components/home/home.component.ts
@@ -9,4 +9,160 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public menus = [
+    {
+      "icon": "mdi mdi-map",
+      "label":"ज़िला",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-map-marker",
+      "label":"तालुका",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-flag",
+      "label":"ग्राम पंचायत",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-flag",
+      "label":"गट ग्राम पंचायत",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-plus",
+      "label":"नविन यूजर जोडा",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-edit",
+      "label":"नविन यूजर एडिट जोडा",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-edit",
+      "label":"नविन यूजर फेरफार जोडा",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-edit",
+      "label":"नविन यूजर फेरफार पीडीएफ जोडा",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-edit",
+      "label":"नविन यूजर वसुली जोडा",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-office-building",
+      "label":"फ्लोर",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-file",
+      "label":"प्रकार",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-file",
+      "label":"मालमत्तेचे प्रकार",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-file",
+      "label":"मिलकत वापर",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-file",
+      "label":"मालमत्ता",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-ticket-percent",
+      "label":"टैक्स",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-ticket-percent",
+      "label":"इतर टैक्स",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-ticket-percent",
+      "label":"एनुअल टैक्स",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-cash-multiple",
+      "label":"ओपन प्लाट रेट्स",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-cash-multiple",
+      "label":"घसारा दर",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-cash-multiple",
+      "label":"भारांक दर",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-radio-tower",
+      "label":"टावर",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-group",
+      "label":"कार्यकारणी कमेटी",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-group",
+      "label":"कामकाज कमेटी",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-google-maps",
+      "label":"मैप",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-file-upload",
+      "label":"अपलोड फाइल",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account-plus",
+      "label":"बी डी ओ नविन जोड़ा",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-image-filter",
+      "label":"सचिव इमेजेस",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-image-filter",
+      "label":"सरपंच इमेजेस",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-image-filter",
+      "label":"उपसरपंच इमेजेस",
+      "url": ""
+    },
+    {
+      "icon": "mdi mdi-account",
+      "label":"एडमिन",
+      "url": ""
+    }
+  ]
 }
+
+//,  , , , , , , , , , , ,
+// , , ,   
+
