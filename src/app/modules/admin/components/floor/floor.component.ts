1a62afe79468bb63bfc9bf9375f97c08155e0674
nitinchandekar <chand.nitin333@gmail.com>
2024-11-25 19:11:09 +0530

    floor Added

--- src/app/modules/admin/components/floor/floor.component.ts
+++ src/app/modules/admin/components/floor/floor.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+import { ToastrService } from 'ngx-toastr';
 import { ITEM_PER_PAGE } from '../../constants/admin.constant';
 import { FloorService } from '../../services/floor.service';
 import Util from '../../utils/utils';
@@ -10,7 +11,7 @@ import { SortingTableComponent } from '../sorting-table/sorting-table.component'
 @Component({
     selector: 'app-floor',
     standalone: true,
-    imports: [SortingTableComponent, PaginationComponent, FormsModule],
+    imports: [SortingTableComponent, PaginationComponent, FormsModule,ReactiveFormsModule],
     templateUrl: './floor.component.html',
     styleUrl: './floor.component.css'
 })
@@ -27,17 +28,25 @@ export class FloorComponent {
 
     keyName: string = 'FLOOR_ID';
     marathiText: string = '';
+    floorFrm = new FormGroup({
+        name: new FormControl<number | null>(null, Validators.required),
+        floor_id: new FormControl<number | string>(''),
 
-    constructor(private titleService: Title, private floorService: FloorService, private util: Util) { }
+    })
+    isSubmitted: boolean = false;
+    isEdit: boolean = false;
+    infoData: any = []
+
+    constructor(private titleService: Title, private floor: FloorService, private util: Util, private toast: ToastrService) { }
 
 
     ngOnInit(): void {
         this.titleService.setTitle('Floor');
-        this.fetchFloorData();
+        this.fetchData();
     }
 
-    fetchFloorData() {
-        this.floorService.getFloorList({ page_number: this.currentPage, search_text: this.searchValue }).subscribe({
+    fetchData() {
+        this.floor.getFloorList({ page_number: this.currentPage, search_text: this.searchValue }).subscribe({
             next: (res: any) => {
                 console.log("res===", res)
                 this.items = res?.data ?? [];
@@ -49,17 +58,102 @@ export class FloorComponent {
         });
     }
 
-    editInfo(id: number) {
+    submit() {
+        this.isSubmitted = true;
+        const data = this.floorFrm.value;
 
+        if (this.floorFrm.invalid) {
+            return;
+        }
+
+        this.floor.addFloor(data).subscribe({
+            next: (res: any) => {
+                if (res.status == 201) {
+
+                    this.toast.success(res?.message, 'Success');
+                    this.isSubmitted = false;
+                    this.fetchData();
+                    this.reset();
+
+                } else {
+                    this.toast.success(res?.message, 'Error');
+                }
+            },
+            error: (err: any) => {
+                this.toast.error('Failed to update floor', 'Error');
+                console.log("error: Update floor ::", err);
+                this.isSubmitted = false;
+            }
+
+        });
+    }
+
+    reset() {
+        this.floorFrm.reset();
+    }
+
+    editInfo(id: number) {
+        this.floor.getFloorById(id).subscribe((res: any) => {
+            this.floorFrm.get('floor_id')?.setValue(res?.data.FLOOR_ID ?? '');
+            this.floorFrm.get('name')?.setValue(res?.data.FLOOR_NAME ?? '');
+            this.isEdit = true;
+        })
+    }
+    update() {
+        this.isSubmitted = true;
+        const data = this.floorFrm.value;
+
+        if (this.floorFrm.invalid) {
+            return;
+        }
+
+        this.floor.updateFloor(data).subscribe({
+            next: (res: any) => {
+                if (res.status == 200) {
+                    this.toast.success(res?.message, 'Success');
+                    this.isSubmitted = false;
+                    this.fetchData();
+                    this.reset();
+
+                } else {
+                    this.toast.success(res?.message, 'Error');
+                }
+            },
+            error: (err: any) => {
+                this.toast.error('Failed to update floor', 'Error');
+                console.log("error: update floor ::", err);
+                this.isSubmitted = false;
+            }
+        });
     }
 
     deleteInfo(id: number) {
-
+        try {
+            this.util.showConfirmAlert().then((res) => {
+                if (!id) {
+                    this.toast.error("Use Id is required", "Error");
+                    return
+                }
+                if (res) {
+                    this.floor.deleteFloor(id).subscribe((res: any) => {
+                        if (res.status == 200) {
+                            this.toast.success(res?.message, 'Success');
+                            this.isSubmitted = false;
+                            this.fetchData()
+                        } else {
+                            this.toast.success(res?.message, 'Error');
+                        }
+                    })
+                }
+            });
+        } catch (error) {
+            console.log("Error:: ", error)
+        }
     }
 
     onPageChange(page: number) {
         this.currentPage = page;
-        this.fetchFloorData();
+        this.fetchData();
     }
 
     translateText(event: Event) {
@@ -74,10 +168,26 @@ export class FloorComponent {
     }
 
     filterData() {
+        this.currentPage = 1;
+        this.debounceFetchDistrictData();
+    }
 
+    private debounceFetchDistrictData = this.debounce(() => {
+        this.fetchData();
+    }, 1000);
+
+    private debounce(func: Function, wait: number) {
+        let timeout: any;
+        return (...args: any[]) => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                func.apply(this, args);
+            }, wait);
+        };
     }
 
     resetFilter(event: Event) {
-
+        this.searchValue = '';
+        this.fetchData();
     }
 }
